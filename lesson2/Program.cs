b51789764af33b68fd5d875ca0420ae18180e582
FeI-VeN <106890844+FeI-VeN@users.noreply.github.com>
2023-04-09 22:18:06 +0300

    Add files via upload

---
+++ lesson2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string  namePlayer;
+            uint    countPlayers;
+            float   healthPlayer;
+            float   damagePlayer;
+            char    symbol;
+            double  armorPlayer;
+            bool    canPlay;
+            byte    rating;
+            long    maxDamage;
+            int     balancePlayerMoney;
+            ushort criticalDamageChance;
+        }
+    }
+}
