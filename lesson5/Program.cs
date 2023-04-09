59c162311dc91a41dd494fd011e0df2c7808df88
FeI-VeN <106890844+FeI-VeN@users.noreply.github.com>
2023-04-09 23:51:23 +0300

    Add files via upload

---
+++ lesson5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name     = "Мамедов";
+            string surname  = "Руслан";
+            Console.WriteLine($"Ваше имя - {name}. Ваша фамилия - {surname}");
+            Console.WriteLine("Пожалуйста нажмите любую коавишу");
+            Console.ReadKey();
+            (name, surname) = (surname, name);
+            Console.WriteLine($"Ваше имя - {name}. Ваша фамилия - {surname}");
+            Console.ReadKey();
+        }
+    }
+}
