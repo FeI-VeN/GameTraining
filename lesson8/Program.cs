5040c3748460458ae716fc3cb2a947959bbb5298
FeI-VeN <106890844+FeI-VeN@users.noreply.github.com>
2023-04-13 21:15:53 +0300

    Add files via upload

---
+++ lesson8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesInHour = 60;
+            int peopleCount;
+            int visitMinutes = 10;
+            int waitingHours;
+            int waitingMinutes;
+            int visitTotalMinutes;
+
+            Console.Write("Введите количество людей в очереди:");
+
+            peopleCount = Convert.ToInt32(Console.ReadLine());
+            visitTotalMinutes = peopleCount * visitMinutes;
+            waitingHours = visitTotalMinutes / minutesInHour;
+            waitingMinutes = visitTotalMinutes % minutesInHour;
+
+            Console.WriteLine($"Вы должны простоять в очереди {waitingHours} часов и {waitingMinutes} минут");
+        }
+    }
+}
