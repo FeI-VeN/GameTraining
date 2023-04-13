6313bb46dd0ecca50f60d249a2736b86d28fad7e
FeI-VeN <106890844+FeI-VeN@users.noreply.github.com>
2023-04-13 20:58:18 +0300

    Add files via upload

---
+++ lesson6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int imagesInRow = 3;
+            int imagesTotal = 52;
+            int rowsTotalOfImages;
+            int remainderOfImages;
+
+            rowsTotalOfImages = imagesTotal / imagesInRow;
+            remainderOfImages = imagesTotal % imagesInRow;
+            Console.WriteLine($"Полностью заполненных рядов картинками - {rowsTotalOfImages};\nСверх меры останется - {remainderOfImages}");
+        }
+    }
+}
